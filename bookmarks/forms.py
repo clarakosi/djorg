b53959a167faa60ad2323e17da27454762b6d8a7
Clara Andrew <candrewwani@gmail.com>
2018-05-10 19:05:54 -0400

    homepage css and delete outline

--- bookmarks/forms.py
+++ bookmarks/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Bookmark
+from .models import Bookmark, PersonalBookmark
 
 class BookmarkForm(forms.ModelForm):
   """Form to create or edit bookmarks."""
   class Meta:
     model = Bookmark
     fields = ('url', 'name', 'notes')
+
+
+class DeleteBookMarkForm(forms.ModelForm):
+  class Meta:
+    model = Bookmark
+    fields = []
--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-from .forms import BookmarkForm
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from .forms import BookmarkForm, DeleteBookMarkForm
 from .models import Bookmark, PersonalBookmark
+import logging
 
 
-
+logger = logging.getLogger(__name__)
 # Create your views here.
 def index(request):
   # import pdb; pdb.set_trace()
@@ -11,6 +13,9 @@ def index(request):
     form = BookmarkForm(request.POST)
     if form.is_valid():
       form.save()
+    else:
+      messages.info(request, "This url bookmark already exists.")
+  
   context = {}
   pb = PersonalBookmark.objects.values_list('id')
   context['bookmarks'] = Bookmark.objects.exclude(id__in=pb)
